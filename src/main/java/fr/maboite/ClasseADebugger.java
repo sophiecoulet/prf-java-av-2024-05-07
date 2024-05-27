4d2d6400ddaa26bf72d1fb2d55e41905e5e83ebb
François Caremoli <francois.caremoli@wanadoo.fr>
2024-05-27 16:16:15 +0200

    Ajout de la correction de Jackson et de la classe à debugger

---
+++ src/main/java/fr/maboite/ClasseADebugger.java
@@ -0,0 +1,15 @@
+package fr.maboite;
+
+public class ClasseADebugger {
+
+	public static void main(String[] args) {
+		boolean isTrue = true;
+		if(isTrue) {
+			System.out.println("Ce programme affichera toujours cette ligne.");
+		}else {
+			System.out.println("Ce programme n'affichera jamais cette ligne.");
+		}
+
+	}
+
+}
---
+++ src/main/java/fr/maboite/correction/MonPojo.java
@@ -0,0 +1,33 @@
+package fr.maboite.correction;
+
+public class MonPojo {
+
+	private String prenom;
+	private String nom;
+	private Integer id;
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+}
